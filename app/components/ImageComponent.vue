92bd9ba3c01cbd5e891f9fe4601de6b07055f2b1
AHOYO Juste <justeahoyo@gmail.com>
2025-11-05 11:40:04 +0100

    Création du component ImageComponent

---
+++ app/components/ImageComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <UCard>
+    <div
+      v-motion
+      :initial="{ y: 0 }"
+      :enter="{
+        y: 10,
+        transition: {
+          y: {
+            repeat: Infinity,
+            duration: 500,
+            repeatType: 'reverse',
+            ease: 'easeInOut',
+          },
+        },
+      }"
+    >
+      <img :src="src" alt="" class="mx-auto" />
+    </div>
+  </UCard>
+</template>
+
+<script setup>
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+});
+</script>
