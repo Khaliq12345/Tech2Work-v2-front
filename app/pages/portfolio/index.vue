f2a7f705c4122f786dd7b10f2558c214ce8c5392
Eldo-Moréo GBOHOUILI <eldomoreogbohouil@gmail.com>
2025-11-05 12:28:24 +0100

    ajout de la page portfolio

---
+++ app/pages/portfolio/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <UPage class="pt-16">
+        <!-- Espace ajouté en haut -->
+        <UPageHeader>
+            <div
+                class="flex flex-col items-center justify-center text-center w-full space-y-3"
+            >
+                <h3 class="text-2xl md:text-3xl font-bold text-primary">
+                    Some of our previous work,
+                </h3>
+                <h3 class="text-xl md:text-2xl font-semibold text-secondary">
+                    Made with care
+                </h3>
+            </div>
+        </UPageHeader>
+        <PortfolioTab />
+    </UPage>
+</template>
