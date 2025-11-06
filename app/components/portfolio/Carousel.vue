83023e6decd8835eb5914d00b1cbaef2dc99baa8
Eldo-Moréo GBOHOUILI <eldomoreogbohouil@gmail.com>
2025-11-06 16:55:57 +0100

    Ajout de la section image

---
+++ app/components/portfolio/Carousel.vue
@@ -0,0 +1,26 @@
+<template>
+    <UPageGrid
+        class="gap-6"
+        :ui="{
+            base: 'grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5',
+        }"
+    >
+        <UCard
+            v-for="(image, index) in images"
+            :key="index"
+            class="overflow-hidden p-0 hover:scale-[1.02] transition-transform duration-300"
+        >
+            <img
+                :src="image"
+                alt="Project image"
+                class="w-full h-full object-cover rounded-lg"
+            />
+        </UCard>
+    </UPageGrid>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    images: string[];
+}>();
+</script>
