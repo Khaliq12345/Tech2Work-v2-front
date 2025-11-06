69ded97705766d4edc397e10f99a23f68f97fafa
AHOYO Juste <justeahoyo@gmail.com>
2025-11-06 14:00:00 +0100

    Création du composant Hero

---
+++ app/components/services/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <UPageHero
+        headline="WHAT WE DO"
+        title="We are here to participate in your business growth."
+        :ui="{
+            title: 'text-left text-black text-2xl sm:text-2xl lg:text-3xl xl:text-4xl font-normal',
+            description:
+                'text-left text-gray-600 dark:text-gray-600 text-base lg:text-lg',
+            links: 'justify-start',
+        }"
+        orientation="horizontal"
+    >
+        <ImageComponent src="/logo.png" class="max-w-sm" />
+    </UPageHero>
+</template>
+
+<script setup lang="ts"></script>
